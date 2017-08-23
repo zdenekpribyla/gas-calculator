eb8a4e478e9bce2ce71a96ec283d3a7c1af3dffb
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-08-23 12:18:31 +0200

    add onclick "Calculate" and keyPress "Enter"

--- app.js
+++ app.js
@@ -1,36 +1,3 @@
-
-//get element by class name
-// get its value
-// zkonvertujes text na number
-// a to vse ulozis do promene
-
-// var numberOfPassengers = document.getElementsByClassName('num-pass-input').value (
-// numberOfPassengers=parseFloat(numberOfPassengers));
-
-// window.onload = function () {
-//
-// }
-
-
-// console.log('testsss')
-
-
-
-// var pozdrav = objekt.greetings('pavel');
-//
-// var objekt = {
-//     name: "zdenek",
-//     age: 33,
-//     getElementsByClassName: function (var1) {
-//         console.log('ahoj ' + var1)
-//     }
-// }
-//
-// var typ = 'age'
-
-
-
-
 var getValues = function () {
     console.log('moje - getValues()');
     return {
@@ -42,115 +9,49 @@ var getValues = function () {
 };
 
 
-//var getNumbers = Number(getValues);
-
-
-// toto na konec var wayBack = document.getElementById('way-back-input');
-
-// vytvori var se string
-// konvertovat na number - novy var
-// a vypsat do console
-//
-// var getValue = '33';
-// console.log('getValue: ' + getValue);
-//
-//
-// var getNumber = Number(getValue);
-// console.log('getNumber: ' + getNumber);
-// console.log('getNumber: ', getNumber);
-
-
-
-// 1. varianta
-// var calculate = function (osoby, cena, prumerna, ujeteKm) {
-//     console.log('Na vylet jsme vyrazili ' + osoby + ' kluci.' )
-//     console.log('Ujeli jsme ' + ujeteKm + ' jen tam.');
-//     console.log('Se zpáteční cestou celkem ' + ujeteKm * 2 + '.')
-//     console.log('Pri cene ' + cena + ' kc za litr jsme utratili za benzin ' + (ujeteKm / 100) * cena * prumerna)
-// };
-//
-// calculate(3, 30, 10, 2000)
-
-// 2. varianta
-
-
-
-
-
-
-var calculate = function(inputData) {
-     console.log('moje - calculate()');
+var calculate = function (inputData) {
+    console.log('moje - calculate()');
     var result = (((inputData.totalDistance / 100) * inputData.gasPrice * inputData.averageConsumption) / inputData.numberOfPassangers);
     console.log(result);
     var wayBack = function () {
         if (document.getElementById('way-back-input').checked) {
-           return result * 2
+            return result * 2
         }
         else {
-           return result
+            return result
         }
+    };
 
-    }
-    { if (wayBack() < 1) {
+    if (wayBack() < 1) {
         document.getElementById('display-result-value').innerHTML = 'Everybody will pay less than 1 CZK.'
     }
     else {
         document.getElementById('display-result-value').innerHTML = 'Everybody will pay ' + Math.floor(wayBack()) + ' CZK.'
-    }};
+    }
 
-
-   // document.getElementById('display-result-value').innerHTML = 'Everybody will pay ' + Math.floor(wayBack()) + ' CZK.'
 
 };
 
+var button = document.getElementById('button-click');
+console.log(button);
+
+button.addEventListener('click', function () {
+    console.log('button-clicked');
+    calculate(getValues())
+    // document.getElementById('display-result-value').innerHTML = calculate(getValues());
+});
 
 
-// document.getElementById('way-back-input').onclick = function () {
-//     console.log()
-//     if (this.checked) {
-//         return result * 2;
-//     }
-//     else {
-//         return result
-//     }
-//
-// };
+// Pro enter pouzit toto: Pavlova verze
+var passangerElement = document.getElementById('num-pass-input');
+console.log('passanger element: ', passangerElement);
 
-
-
-// var check = function () {
-//     document.getElementById(way-back-input).checked = true
-// };
-//
-// var uncheck = function () {
-//     document.getElementById(way-back-input).checked = false
-// };
-
-
-// var parametryJizdy = {
-//     osoby: 2,
-//     cena: 30,
-//     prumerna: 10,
-//     ujeteKm: 1000
-// }
-
-//calculate(getValues())
-
-
-// calculate(parametryJizdy);
-
-
-//nebo
-
-
-
-// var pozdrav = function (jmeno, prijmeni, vek) {
-//     // console.log('ahoj ' + jmeno + ' ' + prijmeni);
-//     // console.log('hello ' + prijmeni + ' ' + vek +' let')
-//     // console.log('Jmenuji se ' + jmeno + ' a je mi ' + vek + ' let.')
-//        console.log(jmeno)
-// };
-//
-// pozdrav('pavel', 'binar', 32);
-// pozdrav('zdenek', 'pribyla', 33);
+passangerElement.addEventListener('keypress', function (event) {
+    var key = event.keyCode;
+    if (key === 13) {
+        console.log('put enter');
+        calculate(getValues())
+    }
+    console.log('event: ', event)
+});
 
