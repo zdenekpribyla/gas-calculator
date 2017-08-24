3942f7d848af48c8601091f1f597de7337246b3d
Zdenek Pribyla <zdenek.pribyla@gmail.com>
2017-08-24 10:37:00 +0200

    add final run and add console.log to each function call

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var getValues = function () {
-    // console.log('moje - getValues()');
+    console.log('getValues()');
+
     return {
         numberOfPassangers: Number(document.getElementById('num-pass-input').value),
         gasPrice: Number(document.getElementById('gas-price-input').value),
@@ -9,16 +10,17 @@ var getValues = function () {
 };
 
 var validate = function (inputData) {
+    console.log('validate()');
 
-    console.log(inputData);
+    // console.log(inputData);
 
-    if (inputData.numberOfPassangers <= 0 || inputData.gasPrice <= 0 || inputData.averageConsumption <= 0 || inputData.gasPrice <= 0) {
-        console.log('numberOfPassangers je 0');
+    if (inputData.numberOfPassangers <= 0 || inputData.gasPrice <= 0 || inputData.averageConsumption <= 0
+        || inputData.totalDistance <= 0) {
+        console.log('cislo je nula nebo nizsi');
         return false
 
-       // document.getElementById('display-result-value').innerHTML = 'Put number higher than 0'
+        // document.getElementById('display-result-value').innerHTML = 'Put number higher than 0'
     }
-
 
     else {
         console.log('numberOfPassangers je OK');
@@ -28,7 +30,8 @@ var validate = function (inputData) {
 
 
 var calculate = function (inputData) {
-    //console.log('moje - calculate()');
+    console.log('calculate()');
+
     var result = (((inputData.totalDistance / 100) * inputData.gasPrice * inputData.averageConsumption)
         / inputData.numberOfPassangers);
     console.log(result);
@@ -40,10 +43,23 @@ var calculate = function (inputData) {
             return result
         }
     };
-
     document.getElementById('display-result-value').innerHTML = 'Everybody will pay ' + Math.round(wayBack() * 100) / 100 + ' CZK.'
+};
 
+var finalRun = function () {
+    console.log('finalRun()');
 
+    var inputData = getValues();
+
+    if (validate(inputData)) {
+        console.log('splnena podminka true - calculate');
+
+        calculate(inputData);
+    }
+    else {
+        alert('You have to put number higher than 0');
+        console.log('false - vyhodi alert');
+    }
 };
 
 //spusteni calculate pres click na button
@@ -51,9 +67,11 @@ var button = document.getElementById('button-click');
 //console.log(button);
 
 button.addEventListener('click', function () {
- //   console.log('button-clicked');
-    calculate(getValues())
+    console.log('button click');
+    //   console.log('button-clicked');
+    finalRun()
 });
+
 
 //loop for inputs class instead of id
 
@@ -71,7 +89,9 @@ for (var index = 0; index < inputsAll.length; ++index) {
     singleInputElement.addEventListener('keypress', function (event) {
         var key = event.keyCode;
         if (key === 13) {
-            calculate(getValues())
+            console.log('keypress enter');
+
+            finalRun()
         }
         // console.log('event: ', event);
     });
@@ -82,6 +102,8 @@ for (var index = 0; index < inputsAll.length; ++index) {
 //wayBack input
 var wayBackElement = document.getElementById('way-back-input');
 wayBackElement.addEventListener('click', function (event) {
+    console.log('wayBackElement click');
+
     calculate(getValues())
 });
 
